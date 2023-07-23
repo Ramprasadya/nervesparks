098ef50e8e3320eb7c98d035d009a3be5b8fcd19
Ramprasadya <yadavramprasad563@gmail.com>
2023-07-23 13:34:23 +0530

    paramas

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/fact/:id', (req, res) => {
+    let num= req.params.id;
+    console.log(num)
+    let i=1;
+    let fact=1;
+    for(i=1;i<=num;i++)
+        fact*=i;
+    let response={"factorial":fact};
+    res.send(response)
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
